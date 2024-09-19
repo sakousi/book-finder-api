62e123d891565b64dcc72bea6e4cc2f5cffc7be8
TristeTempsChang <tristan.tran2111@gmail.com>
2024-09-19 10:19:20 +0200

    add books on api and database

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { Book } from './entities/book.entity';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('Book')
 @Controller('book')
@@ -10,27 +12,37 @@ export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Post()
-  create(@Body() createBookDto: CreateBookDto) {
-    return this.bookService.create(createBookDto);
+  @UseInterceptors(FileInterceptor('book_image'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Create book',
+    type: CreateBookDto,
+  })
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() createBookDto: CreateBookDto,
+  ) {
+    const filePath = file ? `/uploads/${file.filename}` : null;
+    return this.bookService.create({ ...createBookDto, book_image: filePath });
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<Book[]> {
     return this.bookService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string): Promise<Book> {
     return this.bookService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
+  async update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto): Promise<Book> {
     return this.bookService.update(+id, updateBookDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string): Promise<void> {
     return this.bookService.remove(+id);
   }
 }
---
+++ src/typeorm/enum/bookGenre.ts
@@ -0,0 +1,14 @@
+export enum BookGenre {
+    fantasy = 'Fantaisie',
+    adventureFiction = 'Aventure de fiction',
+    literaryFiction = 'Fiction littéraire',
+    mystery = 'Mystère',
+    romanceNovel = 'Roman sentimental',
+    scienceFiction = 'Science fiction',
+    thriller = 'Thriller',
+    childrenLiterature = 'Littérature pour enfants',
+    classics = 'Classique',
+    historicalFiction = 'Fiction historique',
+    horror = 'Horreur'
+}
+  
